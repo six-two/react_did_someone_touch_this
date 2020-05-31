745e67d2057a3e82bd82093bda1aebbf373ec598
six-two <info@six-two.dev>
2020-05-31 14:34:31 +1200

    Updates

---
+++ precache-manifest.47c63eaf677c20235cb122c757badfcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f597a85c2d7aeff2711af403eb881c2",
+    "url": "/react_did_someone_touch_this/index.html"
+  },
+  {
+    "revision": "0edf4852b298270a0647",
+    "url": "/react_did_someone_touch_this/static/css/main.a620b0bf.chunk.css"
+  },
+  {
+    "revision": "b079757843049b1b5d8b",
+    "url": "/react_did_someone_touch_this/static/js/2.fc502683.chunk.js"
+  },
+  {
+    "revision": "b33aea1627a88fcd0bb458fe135ac495",
+    "url": "/react_did_someone_touch_this/static/js/2.fc502683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edf4852b298270a0647",
+    "url": "/react_did_someone_touch_this/static/js/main.d377609f.chunk.js"
+  },
+  {
+    "revision": "238d576cd5264d859924",
+    "url": "/react_did_someone_touch_this/static/js/runtime-main.d4490ceb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_did_someone_touch_this/precache-manifest.ac4140e56433f0923913e8c097147713.js"
+  "/react_did_someone_touch_this/precache-manifest.47c63eaf677c20235cb122c757badfcc.js"
 );
 
 self.addEventListener('message', (event) => {
